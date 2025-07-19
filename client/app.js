7de0b3baf0447e2ee867c4ca9ac3e5f0312805e9
GojurJeevan <jeevangojur2605@gmail.com>
2025-07-19 20:48:00 +0530

    Load from Storage

--- client/app.js
+++ client/app.js
@@ -31,4 +31,31 @@ function saveTasks() {
   });
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.forEach(task => {
+    const li = document.createElement('li');
+    li.textContent = task.text;
+
+    if (task.done) li.classList.add('done');
+
+    li.addEventListener('click', () => {
+      li.classList.toggle('done');
+      saveTasks();
+    });
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '🗑';
+    deleteBtn.onclick = () => {
+      li.remove();
+      saveTasks();
+    };
+    li.appendChild(deleteBtn);
+
+    document.getElementById('taskList').appendChild(li);
+  });
+}
+
+window.onload = loadTasks;
+
 
